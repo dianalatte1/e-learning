1e6af08e40ce5ed5dc3b34e3b587c644e99b26c4
Diana <ada_wit60@hackademy.mx>
2022-03-04 22:23:38 -0600

    Implement  Hooks with Camera Component

--- screens/TransactionScreen.js
+++ screens/TransactionScreen.js
@@ -1,10 +1,41 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { Camera, getCameraPermissionsAsync } from "expo-camera";
 
 const TransactionScreen = (props) => {
+  // Los permisos de la camara
+  const [hasPermission, setHasPermission] = useState(null);
+  // para saber si es modo escanear o escaneando
+  const [domState, setDomState] = useState("normal");
+  // para saber si el escaneo esta completo
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+      // estas dos lineas estan bien?
+      setDomState();
+      setScanned();
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Pantalla de Transaccion</Text>
+      <TouchableOpacity
+        style={styles.button}
+        // onPress={() => setHasPermission('scanner')} ?????????
+      >
+        <Text style={styles.text}>Escanear QR</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -13,11 +44,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "blue",
+    backgroundColor: "#54c8ac",
   },
   text: {
     color: "white",
-    fontSize: 30,
+    fontSize: 15,
+  },
+  button: {
+    backgroundColor: "#f49e7e",
+    height: 55,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 15,
+    width: "43%",
   },
 });
 

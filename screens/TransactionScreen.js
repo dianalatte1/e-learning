998f6b98dfe9c21d7ba27ba4635494015f284093
Diana Bautista <ing.diane.bb@gmail.com>
2022-03-21 15:56:52 -0600

    This is the template without activity comments

--- screens/TransactionScreen.js
+++ screens/TransactionScreen.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
-import { Camera, getCameraPermissionsAsync } from "expo-camera";
+import { Camera } from "expo-camera";
+
 const TransactionScreen = (props) => {
   // Los permisos de la camara
   const [hasPermission, setHasPermission] = useState(null);
@@ -10,12 +11,24 @@ const TransactionScreen = (props) => {
   // para saber si el escaneo esta completo
   const [scanned, setScanned] = useState(false);
   const [scannedData, setScannedData] = useState("");
+  // agregando los estudiantes y los libros
+  const [studentId, setStudentId] = useState("");
+  const [bookId, setBookId] = useState(""); // the id of the scanned book
 
   const saveBarcodeData = async ({ type, data }) => {
     console.log(data); // show the barcode in the terminal
     setScannedData(data);
-    setDomState("normal");
-    setScanned(true);
+    // agregando la validacion para escanear un libro o un estudiante
+    if (domState === "SCAN_BOOK") {
+      // please scan a book
+      setBookId(data);
+      setDomState("normal");
+      setScanned(true);
+    } else if (domState === "SCAN_STUDENT") {
+      setStudentId(data);
+      setDomState("normal");
+      setScanned(true);
+    }
   };
 
   useEffect(() => {
@@ -28,21 +41,60 @@ const TransactionScreen = (props) => {
   if (hasPermission === false) {
     return <Text>No access to camera</Text>;
   }
-
-  return domState !== "scanner" ? (
+  // si el estado no es normal muestrame el escaner
+  if (domState != "normal") {
+    return (
+      <BarCodeScanner
+        style={StyleSheet.absoluteFillObject}
+        onBarCodeScanned={scanned ? undefined : saveBarcodeData}
+      />
+    );
+  }
+  return (
     <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => setDomState("scanner")}
-      >
-        <Text style={styles.text}>Escanear QR</Text>
-      </TouchableOpacity>
+      {/* Agregamos imagen de fondo */}
+      <ImageBackground source={bgImage} style={styles.bgImage}>
+        <View style={styles.upperContainer}>
+          {/* En este contenedr colocamos el icono/nombre de la app */}
+          <Image source={appIcon} style={styles.appIcon} />
+          <Image source={appName} style={styles.appName} />
+        </View>
+        <View style={styles.lowerContainer}>
+          {/* este contenedor tendra un textInput y un button */}
+          <View style={styles.textinputContainer}>
+            <TextInput
+              style={styles.textinput}
+              placeholder={"Id del libro"}
+              placeholderTextColor={"#FFFFFF"}
+              value={bookId}
+              onChange={(e) => setBookId(e.target.value)}
+            />
+            <TouchableOpacity
+              style={styles.scanbutton}
+              onPress={() => setDomState("SCAN_BOOK")}
+            >
+              <Text style={styles.scanbuttonText}>Escanear</Text>
+            </TouchableOpacity>
+          </View>
+          {/* este contenedor tendra un textInput y un button */}
+          <View style={[styles.textinputContainer, { marginTop: 25 }]}>
+            <TextInput
+              style={styles.textinput}
+              placeholder={"Id del alumno"}
+              placeholderTextColor={"#FFFFFF"}
+              value={studentId}
+              onChange={(e) => setStudentId(e.target.value)}
+            />
+            <TouchableOpacity
+              style={styles.scanbutton}
+              onPress={() => setDomState("SCAN_STUDENT")}
+            >
+              <Text style={styles.scanbuttonText}>Escanear</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
     </View>
-  ) : (
-    <BarCodeScanner
-      style={StyleSheet.absoluteFillObject}
-      onBarCodeScanned={scanned ? undefined : saveBarcodeData}
-    />
   );
 };
 const styles = StyleSheet.create({
